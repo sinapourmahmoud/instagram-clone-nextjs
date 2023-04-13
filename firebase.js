09d059dab96c9287e862bd4214c3f3706fcb1365
sinapourmahmoud <sinapoormahmood@gmail.com>
2023-04-13 18:29:42 +0430

    adding firebase and handle login page

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+};
+
+// Initialize Firebase
+export const app = !getApps() ? initializeApp(firebaseConfig) : getApp();
+export const db = getFirestore();
+export const storage = getStorage();
---
+++ pages/auth/login.jsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import React from "react";
+import { getProviders, signIn as signIntoProvider } from "next-auth/react";
+const Login = ({ providers: { google } }) => {
+  return (
+    <div
+      key={google.name}
+      className="flex flex-col items-center gap-6 py-5 w-screen h-screen justify-center"
+    >
+      <Image
+        src="https://links.papareact.com/ocw"
+        alt="logo"
+        objectFit="contain"
+        width={135}
+        height={135}
+      />
+      <button
+        onClick={() => {
+          signIntoProvider(google.id);
+        }}
+        className="bg-blue-500 text-white py-2 px-4 rounded-3xl"
+      >
+        Sign in with {google.name}
+      </button>
+    </div>
+  );
+};
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+}
+export default Login;
